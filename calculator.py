4aa6e69ce6a63949964477810c6bb2b70b30031b
davidkaggulire <dkaggulire@gmail.com>
2018-11-27 23:33:08 +0300

    Adds calculator functions

---
+++ calculator.py
@@ -0,0 +1,31 @@
+"""calculator.py"""
+
+
+def add_function(first_num, second_num):
+    """function adds two numbers"""
+    addition = first_num + second_num
+    return addition
+
+
+def subtract_function(first_num, second_num):
+    """function subtracts two numbers"""
+    difference = first_num-second_num
+    return difference
+
+
+def division_function(first_num, second_num):
+    """function divides two numbers"""
+    dividend = first_num / second_num
+    return dividend
+
+
+def multiplication_function(first_num, second_num):
+    """function multiplies two numbers"""
+    product = first_num * second_num
+    return product
+
+
+def remainder_function(first_num, second_num):
+    """function to give the remainder of two numbers"""
+    remainder = first_num % second_num
+    return remainder
